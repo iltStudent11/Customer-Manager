99a76f8c631c55b6607384aa2af1cc5832ea1709
Mike Williams <Mike.Williams@libertyMutual.com>
2026-07-08 17:18:24 +0000

    fixed deploy issue

--- src/customer-app/src/main.tsx
+++ src/customer-app/src/main.tsx
@@ -8,7 +8,7 @@ import './styles.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CustomerProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </CustomerProvider>
